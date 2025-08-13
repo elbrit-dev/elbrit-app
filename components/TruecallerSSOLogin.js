d49a673fc8cd5902e3a119a00136bd0641ce45e4
barani0709 <baranidharan@elbrit.org>
2025-08-13 12:23:30 +0530

    Add netlify

--- components/TruecallerSSOLogin.js
+++ components/TruecallerSSOLogin.js
@@ -200,7 +200,7 @@ const TruecallerComponent = ({
             phase: "response",
             requestId: nonce,
             attempt: retryCountRef.current + 1,
-            status: status || "unknow",
+            status: status || "unknown",
             data,
           });
         }
